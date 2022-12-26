c7170611c1129e7886381bc83cf325233831ac90
MrDavudov <davruslan90@gmail.com>
2022-12-26 19:38:53 +0300

    add

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+    "github.com/mehanizm/airtable"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+
+    client := airtable.NewClient(os.Getenv("API"))
+
+    table := client.GetTable("appTh2xBTnix4oi1d", "Features")
+
+    records, err := table.GetRecords().
+	FromView("Table view").
+	ReturnFields("CODE" ,"Name").
+	InStringFormat("Europe/Moscow", "ru").
+	Do()
+    if err != nil {
+        panic(err)
+    }
+
+	fmt.Println("Best Public Domain Books: ")
+
+	for _, tableRecord := range records.Records {
+        fmt.Println(tableRecord.Fields["CODE"])
+    }
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/MrDavudov/AirtableToDraw
+
+go 1.19
+
+require (
+	github.com/brianloveswords/airtable v0.0.0-20201104232343-083b90826e4a // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/mehanizm/airtable v0.2.6 // indirect
+	go.uber.org/ratelimit v0.1.0 // indirect
+)
